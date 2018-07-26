e1135decd2dd2b8f4291eddfd33bd1876d622415
Andrew Kaiser <akaiser@wayfair.com>
2018-07-26 18:59:20 -0400

    allow for input changes

--- index.js
+++ index.js
@@ -1,56 +1,147 @@
-const config = {
-  scrape: {
-    name: "mainpage",
-    download: "mainpage",
-    parse: "batchID",
-    scrapeEach: [
-      {
-        name: "grid",
-        download: "batchPage{batchID}",
-        scrapeNext: {
-          name: "id-grabber",
-          parse: "batchID"
-        },
-        scrapeEach: [
-          {
-            name: "tag",
-            parse: "span.tag"
-          },
-          {
-            name: "parse-image-page",
-            parse: "li a.images",
-            scrapeEach: [
-              {
-                name: "image-page",
-                download: "imagepage{imageID}",
-                parse: "img",
-                scrapeEach: [
-                  {
-                    name: "image",
-                    download: "{src}"
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    ]
+const traverse = (root, func, i = 0) => {
+  func(root, i);
+  if (root.children) {
+    for (let i = 0; i < root.children.length; i++) {
+      traverse(root.children[i], func, i);
+    }
   }
 };
-
 const downloadText = configStep => {
-  const download = configStep.download ? "Download" : "Identity";
-  const content = configStep.parse ? "html" : "file";
+  const download = configStep.download ? "Download" : "identity(x) = x";
+  const content = configStep.parse ? "HTML" : "file";
   return download;
 };
 const parseText = configStep => {
   if (configStep.parse) {
-    const content = "html";
+    const content = configStep.parse.expect.toUpperCase();
     return `Parse ${content}`;
   } else {
-    return "Identity";
+    return "identity(x) = x";
   }
+};
+
+const update = ({
+  rectNode,
+  get,
+  initialWidth,
+  initialHeight,
+  baseSvg,
+  margin,
+  nodeGroup,
+  linkGroup,
+  diagonal
+}) => config => {
+  let width = initialWidth,
+    height = initialHeight;
+  const arrowSize = 12;
+  const downloadToParse = 20;
+
+  const root = d3.hierarchy(config.scrape, step => [
+    ...(step.scrapeEach || []),
+    ...(step.scrapeNext ? [step.scrapeNext] : [])
+  ]);
+  const treemap = d3.tree().size([500, initialWidth]);
+  treemap(root);
+  traverse(root, (d, i) => {
+    d.y = d.depth * (rectNode[get.height] * 1.5);
+    // d.x = d.x + d.depth * 17 // make arrow start at next parse
+    if (d.y + rectNode.width > width) width = d.y + rectNode.width;
+    if (d.x + rectNode.height > height) height = d.x;
+  });
+  const descendants = root.descendants().sort((a, b) => b.y - a.y); // sort for use w/ scrapeNext
+  const links = root.links().map(d => ({
+    ...d,
+    source: {
+      ...d.source,
+      y: d.source.y + rectNode.width, // make the line start after the rectangle
+      x: d.source.x + downloadToParse
+    },
+    target: {
+      ...d.target,
+      y: d.target.y - arrowSize, // space for arrow
+      x: d.target.x
+    }
+  }));
+  const incrementers = descendants.filter(
+    d => d.data.download && d.data.download.increment
+  );
+
+  baseSvg
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom);
+
+  // Nodes
+  const gNodeAll = nodeGroup.selectAll("g").data(descendants);
+
+  const gNode = gNodeAll
+    .enter()
+    .append("g")
+    .merge(gNodeAll)
+    .attr(
+      "transform",
+      d => `translate(${d[get.x]}, ${d[get.y] - rectNode.height / 2})`
+    );
+  gNodeAll.exit().remove();
+
+  // Labels
+  const labelDiv = gNode
+    .merge(gNodeAll)
+    .append("foreignObject")
+    .classed("label", true)
+    .attr("width", rectNode.width)
+    .attr("height", rectNode.height)
+    .append("xhtml:div")
+    .classed("scrape-step", true)
+    .attr("title", d => d.data.name);
+  labelDiv
+    .append("div")
+    .classed("name", true)
+    .text(d => d.data.name);
+  labelDiv
+    .append("div")
+    .classed("download", true)
+    .classed("identity", d => !d.data.download)
+    .text(d => downloadText(d.data));
+  labelDiv
+    .append("div")
+    .classed("parse", true)
+    .classed("identity", d => !d.data.parse)
+    .text(d => parseText(d.data));
+
+  // Increment Links
+  const curveHeight = 30;
+  const curvePull = downloadToParse;
+  gNode
+    .filter(d => d.data.download && d.data.download.increment)
+    .append("g")
+    .classed("link", true)
+    .append("path")
+    .attr(
+      "d",
+      d =>
+        `M ${rectNode.width} ${rectNode.height / 2 + downloadToParse}
+      C ${rectNode.width + curvePull} ${rectNode.height / 2 +
+          downloadToParse} ${rectNode.width +
+          curvePull * 2} ${-curveHeight} ${rectNode.width / 2} ${-curveHeight}
+      C ${-curvePull - arrowSize - 10} ${-curveHeight} ${-curvePull -
+          arrowSize} ${rectNode.height / 2} ${-arrowSize} ${rectNode.height /
+          2}`
+    );
+  // scrapeNext Links
+  // sort descendants by y
+  // for scrapeNext, find max x of y's that are greater than it
+
+  // Links
+  const linksAll = linkGroup.selectAll("path").data(links);
+  linksAll
+    .enter()
+    .append("g")
+    .merge(linksAll)
+    .classed("link", true)
+    .append("path")
+    .attr("d", diagonal)
+    .attr("marker-end", "url(#end-arrow)");
+  linksAll.exit().remove();
 };
 
 window.onload = () => {
@@ -62,12 +153,12 @@ window.onload = () => {
     height: "width"
   };
 
-  const margin = { top: 20, right: 50, bottom: 100, left: 20 };
+  const margin = { top: 0, right: 20, bottom: 100, left: 20 };
   // dynamically set these later
-  let width = 500,
-    height = 500;
+  let width = 200;
+  height = 100;
   // elements
-  const rectNode = { width: 120, height: 60, textMargin: 5 };
+  const rectNode = { width: 140, height: 75, textMargin: 5 };
   const baseSvg = d3
     .select("#tree-container")
     .append("svg")
@@ -77,6 +168,21 @@ window.onload = () => {
     .append("g")
     .attr("class", "drawarea")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  const defsGroup = baseSvg.append("svg:defs");
+  const defs = {
+    arrow: defsGroup
+      .append("marker")
+      .attr("id", "end-arrow")
+      .attr("viewBox", "0 -5 10 10")
+      .attr("refX", 0)
+      .attr("refY", 0)
+      .attr("markerWidth", 6)
+      .attr("markerHeight", 6)
+      .attr("orient", "auto")
+      .attr("class", "arrow")
+      .append("path")
+      .attr("d", "M0,-5L10,0L0,5")
+  };
 
   const nodeGroup = svgGroup.append("g").attr("id", "nodes");
   const linkGroup = svgGroup.append("g").attr("id", "links");
@@ -85,150 +191,30 @@ window.onload = () => {
     .x(d => d[get.x])
     .y(d => d[get.y]);
 
-  const root = d3.hierarchy(config.scrape, step => [
-    ...(step.scrapeEach || []),
-    ...(step.scrapeNext ? [step.scrapeNext] : [])
-  ]);
-  const treemap = d3.tree().size([400, 250]);
-  treemap(root);
-
   // update function, call it on text box change
-  const update = root => {
-    const traverse = (root, func, i = 0) => {
-      func(root, i);
-      if (root.children) {
-        for (let i = 0; i < root.children.length; i++) {
-          traverse(root.children[i], func, i);
-        }
-      }
-    };
-    traverse(root, (d, i) => {
-      d.y = d.depth * (rectNode[get.height] * 1.5);
-      if (d.y > width) width = d.y + rectNode.width;
-    });
-    const descendants = root.descendants();
-    const links = root.links();
+  const updater = update({
+    rectNode,
+    get,
+    initialWidth: width,
+    initialHeight: height,
+    baseSvg,
+    margin,
+    nodeGroup,
+    linkGroup,
+    diagonal
+  });
+  updater(config);
 
-    baseSvg
-      .attr("width", width + margin.right + margin.left)
-      .attr("height", height + margin.top + margin.bottom);
-
-    // Nodes
-    gNode = nodeGroup
-      .selectAll("g.node")
-      .data(descendants)
-      .enter()
-      .append("g")
-      .attr(
-        "transform",
-        d => `translate(${d[get.x]}, ${d[get.y] - rectNode.height / 2})`
-      );
-
-    gNode
-      .append("rect")
-      .classed("node", true)
-      .attr("rx", 4)
-      .attr("ry", 4)
-      .attr("width", rectNode.width)
-      .attr("height", rectNode.height);
-
-    // Labels
-    const labelDiv = gNode
-      .append("foreignObject")
-      .classed("label", true)
-      .attr("width", () => rectNode.width)
-      .attr("height", () => rectNode.height)
-      .append("xhtml:div");
-    labelDiv.classed("scrape-step", true);
-
-    labelDiv
-      .append("div")
-      .classed("name", true)
-      .text(d => d.data.name);
-    labelDiv
-      .append("div")
-      .classed("download", true)
-      .text(d => downloadText(d.data));
-
-    labelDiv
-      .append("div")
-      .classed("parse", true)
-      .text(d => parseText(d.data));
-
-    // Links
-    linkGroup
-      .selectAll("path.link")
-      .data(root.links())
-      .enter()
-      .append("g")
-      .classed("link", true)
-      // .append("path")
-      // .attr("d", diagonal);
-      .append("line")
-      .attr("x1", d => d.source[get.x] + rectNode.width)
-      .attr("y1", d => d.source[get.y])
-      .attr("x2", d => d.target[get.x])
-      .attr("y2", d => d.target[get.y]);
+  const userInput = document.querySelector("#config-input");
+  userInput.placeholder = JSON.stringify(config, null, 2);
+  const updateFromInput = e => {
+    try {
+      const newConfig = JSON.parse(e.target.value);
+      updater(newConfig);
+    } catch (e) {
+      if (e.name !== "SyntaxError") throw e;
+    }
   };
-  update(root);
-  // const descendants = root.descendants();
-  // const links = root.links();
-
-  // const diagonal = d3
-  // .linkHorizontal()
-  // .x(d => d.y)
-  // .y(d => d.x);
-
-  // // Nodes
-  // d3.select("svg g.nodes")
-  // .selectAll("circle.node")
-  // .data(root.descendants())
-  // .enter()
-  // .append("g")
-  // .attr("transform", d => "translate(" + d.y + "," + d.x + ")")
-  // .append("circle")
-  // .classed("node", true)
-  // // .attr("cx", d => d.x)
-  // // .attr("cy", d => d.y)
-  // .attr("r", 4);
-
-  // // Links
-  // d3.select("svg g.links")
-  // .selectAll("path.link")
-  // .data(root.links())
-  // .enter()
-  // .append("g")
-  // .append("path")
-  // .classed("link", true)
-  // .attr("d", diagonal);
-
-  // // Labels
-  // d3.select("svg g.labels")
-  // .selectAll("text")
-  // .data(descendants)
-  // .enter()
-  // .append("text")
-  // .classed("label", true)
-  // .attr("alignment-baseline", "middle")
-  // .attr("x", d => d.y)
-  // .attr("y", d => d.x)
-  // .text(d => d.data.name)
-  // .each(function(d, i) {
-  // descendants[i].bb = this.getBBox();
-  // });
-
-  // // Label Boxes
-  // const padding = 6;
-  // d3.select("svg g.label-boxes")
-  // .selectAll("text")
-  // .data(descendants)
-  // .enter()
-  // // .append("g")
-  // // .attr("transform", d => "translate(" + d.y + "," + d.x + ")")
-  // .append("rect")
-  // .classed("box", true)
-  // .attr("x", (d, i) => d.y - descendants[i].bb.width / 2 - padding / 2)
-  // .attr("y", (d, i) => d.x - descendants[i].bb.height + padding / 2)
-  // .attr("width", (d, i) => descendants[i].bb.width + padding)
-  // .attr("height", (d, i) => descendants[i].bb.height + padding);
+  userInput.onchange = updateFromInput;
+  userInput.onkeyup = updateFromInput;
 };
